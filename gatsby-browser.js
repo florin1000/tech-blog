acd184a4321455bc5917d06592520c6a73ea23c3
florin <florin.trade101@gmail.com>
2025-02-19 12:38:02 +0200

    initial commit

---
+++ gatsby-browser.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { ThemeProvider } from "theme-ui";
+import theme from "./src/gatsby-plugin-theme-ui";
+// import "prismjs/themes/prism.css" //default style
+import "prismjs/themes/prism-tomorrow.css";//prism-okaidia/prism-coy/prism-solarized
+
+import "./src/styles/prism-custom.css";
+
+// custom typefaces
+import "@fontsource-variable/montserrat"
+import "@fontsource/merriweather"
+// normalize CSS across browsers
+import "./src/normalize.css"
+// custom CSS styles
+import "./src/style.css"
+
+import "@fontsource/ibm-plex-sans/400.css"; // for body
+import "@fontsource/ibm-plex-sans/700.css"; // for headings
+import "@fontsource/ibm-plex-mono/400.css"; // for code
+
+export const wrapRootElement = ({ element }) => (
+  <ThemeProvider theme={theme}>{element}</ThemeProvider>
+);
